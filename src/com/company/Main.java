0657bbdb4bfd69fa729995c5305903e20395c3bc
imvickykumar <vickythetechguy@gmail.com>
2018-09-30 19:02:50 +0530

    All Types Pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+
         for(int i=0;i<5;i++){
             for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println(" ");
         }
-
 
         System.out.println("##############################################");
 
@@ -21,7 +21,6 @@ public class Main {
             }
             System.out.println(" ");
         }
-
 
         System.out.println("##############################################");
 
@@ -38,6 +37,71 @@ public class Main {
 
         System.out.println("##############################################");
 
+        for(int i=0;i<5;i++){
 
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=5;k>i;k--){
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+
+        System.out.println("##############################################");
+
+        for(int i=1; i<=5; i++){
+
+            for(int k=5;k>i;k--){
+                System.out.print(" ");
+            }
+
+            for(int j=0;j<(i*2)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("##############################################");
+
+        for(int i=5; i>=1; i--){
+
+            for(int k=5;k>i;k--){
+                System.out.print(" ");
+            }
+
+            for(int j=(i*2)-1;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("##############################################");
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+
+                System.out.print(j+" ");
+            }
+            System.out.println(" ");
+        }
+
+        System.out.println("##############################################");
+
+        //Floyd's Triangle;
+
+        int num=1;
+
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<i;j++){
+
+                System.out.print(num+" ");
+                ++num;
+            }
+            System.out.println(" ");
+        }
+
+        System.out.println("##############################################");
     }
 }
